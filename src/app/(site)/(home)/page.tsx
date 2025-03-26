b4f1738900714d9f0d6e9d3c57edcf0761cf136e
Victor Oliveira <jvictorddo@gmail.com>
2025-03-26 19:28:32 +0000

    feat: connect PostgreSQL database to application using Drizzle ORM
    - Initialized Neon/PostgreSQL connection
    - Set up Drizzle ORM with pgTableCreator and schema
    - Connected database to application context

--- src/app/(site)/(home)/page.tsx
+++ src/app/(site)/(home)/page.tsx
@@ -6,10 +6,14 @@ import { HydrateClient, api } from "@/trpc/server";
 import { mockImages, type MockImage } from "@/utils/mockImages";
 import Image from "next/image";
 import { Page } from "../../_components/layout/Page";
+import { db } from "@/server/db";
 
 export default async function Home() {
 	const hello = await api.post.hello({ text: "from tRPC" });
 	const session = await auth();
+
+	const posts = await db.query.posts.findMany();
+	console.log(posts);
 
 	if (session?.user) {
 		void api.post.getLatest.prefetch();
@@ -18,6 +22,21 @@ export default async function Home() {
 	return (
 		<HydrateClient>
 			<div className=" grid grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-2 p-2">
+				{posts.map(
+					(post: {
+						id: number;
+						name: string | null;
+						createdById: string;
+						createdAt: Date;
+						updatedAt: Date | null;
+					}) => {
+						return (
+							<div key={post.id} className="relative aspect-square w-full ">
+								{post.name}
+							</div>
+						);
+					},
+				)}
 				{mockImages.map((image: MockImage) => {
 					return (
 						<div key={image.key} className="relative aspect-square w-full ">
