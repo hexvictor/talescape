0f00de8188a698b49c7b5244f3ea921055c5107a
Victor Oliveira <jvictorddo@gmail.com>
2025-03-30 05:56:28 +0100

    fix: user first name fix and testing webhooks environment variables

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -14,12 +14,12 @@ export async function POST(req: NextRequest) {
 		? process.env.CLERK_WEBHOOK_SECRET_DEV
 		: process.env.CLERK_WEBHOOK_SECRET_PROD;
 
-	if (!secret) {
+	if (!process.env.CLERK_WEBHOOK_SECRET_PROD) {
 		console.error("Missing Clerk webhook secret");
 		return new Response("Server misconfigured", { status: 500 });
 	}
 
-	const wh = new Webhook(secret);
+	const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET_PROD);
 
 	// biome-ignore lint/suspicious/noExplicitAny: webhook type is dynamic
 	let evt: any;
@@ -37,21 +37,12 @@ export async function POST(req: NextRequest) {
 		const { id, email_addresses, username, image_url, first_name, last_name } =
 			eventData;
 
-		// id: d.varchar({ length: 255 }).notNull().primaryKey(), // Clerk user ID
-		// username: d.varchar({ length: 255 }),
-		// firstName: d.varchar({ length: 255 }),
-		// lastName: d.varchar({ length: 255 }),
-		// name: d.varchar({ length: 255 }),
-		// email: d.varchar({ length: 255 }).notNull(),
-		// emailVerified: d.timestamp({ withTimezone: true }),
-		// image: d.varchar({ length: 1024 }),
-
 		await db.insert(users).values({
 			id,
 			email: email_addresses[0]?.email_address ?? "",
 			username: username,
 			name: `${first_name ?? ""} ${last_name ?? ""}`.trim(),
-			firstName: `${first_name ?? ""}}`.trim(),
+			firstName: `${first_name ?? ""}`.trim(),
 			lastName: `${last_name ?? ""}`.trim(),
 			image: image_url,
 			emailVerified: new Date(),
