868bfdadc1de136a022f93f4f66feac892244abb
Victor Oliveira <jvictorddo@gmail.com>
2025-04-17 08:36:03 +0100

    feat: add a discreet with dynamic year and version info

--- src/app/_components/layout/Footer/Footer.tsx
+++ src/app/_components/layout/Footer/Footer.tsx
@@ -1,7 +1,8 @@
+import { version } from "@package.json";
 export default function Footer() {
 	return (
-		<div className="flex min-h-10 items-center justify-center bg-black text-white">
-			Created by: HexVictor
+		<div className="flex min-h-10 items-center justify-center bg-black py-2 text-center text-gray-400 text-xs">
+			© {new Date().getFullYear()} Talescape — v{version}
 		</div>
 	);
 }
